8d93807dafcf87aa16de520d99db873c76f95e2b
Chanon Navajongpun <chanon.na@ku.th>
2022-09-09 12:36:55 +0700

    add validation

--- src/main/java/th/ac/ku/restaurant/model/User.java
+++ src/main/java/th/ac/ku/restaurant/model/User.java
@@ -1,10 +1,15 @@
 package th.ac.ku.restaurant.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.UUID;
 
+@Data
+@NoArgsConstructor
 @Entity
 public class User {
 
@@ -17,53 +22,9 @@ public class User {
     private String firstName;
     private String lastName;
     private String role;
+    private String email;
 
-    public UUID getId() {
-        return id;
-    }
 
-    public String getUsername() {
-        return username;
-    }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
 
 }
