88e3abebdb64e64b93efd2a8247157f769d97a3e
7842 <chanon.na@ku.th>
2022-09-09 18:37:44 +0700

    add api and update role

--- src/main/java/th/ac/ku/restaurant/controller/MenuController.java
+++ src/main/java/th/ac/ku/restaurant/controller/MenuController.java
@@ -3,9 +3,14 @@ package th.ac.ku.restaurant.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import th.ac.ku.restaurant.dto.MenuDto;
 import th.ac.ku.restaurant.service.MenuService;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/menu")
@@ -19,5 +24,19 @@ public class MenuController {
         model.addAttribute("menus", service.getMenus());
         return "menu";
     }
-}
 
+    @GetMapping("/add")
+    public String getMenuForm(MenuDto menuDto) {
+        return "menu-add";
+    }
+
+    @PostMapping("/add")
+    public String addMenu(@Valid MenuDto menuDto, BindingResult result,
+                          Model model) {
+        if (result.hasErrors())
+            return "menu-add";
+
+        service.addMenu(menuDto);
+        return "redirect:/menu";
+    }
+}
--- src/main/java/th/ac/ku/restaurant/service/MenuService.java
+++ src/main/java/th/ac/ku/restaurant/service/MenuService.java
@@ -1,10 +1,7 @@
 package th.ac.ku.restaurant.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import th.ac.ku.restaurant.dto.MenuDto;
@@ -39,5 +36,24 @@ public class MenuService {
         MenuDto[] menus = response.getBody();
         return Arrays.asList(menus);
     }
+
+    public MenuDto addMenu(MenuDto menu) {
+
+        String token = tokenService.requestAccessToken();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("authorization", "Bearer " + token);
+        headers.add("Content-Type", MediaType.APPLICATION_JSON.toString());
+        HttpEntity entity = new HttpEntity(menu,headers);
+
+        String url = "http://localhost:8090/menu";
+
+        ResponseEntity<MenuDto> response =
+                restTemplate.exchange(url, HttpMethod.POST,
+                        entity, MenuDto.class);
+
+        return response.getBody();
+    }
+
 }
 
